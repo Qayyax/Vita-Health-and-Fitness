88ed456e4f6527d36d2d113b0f847b6f0f39edf7
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-08-23 10:32:27 -0400

    Initial Commit

---
+++ Vita-Health/Vita_HealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vita_HealthApp.swift
+//  Vita-Health
+//
+//  Created by Qayyax on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Vita_HealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Vita-HealthTests/Vita_HealthTests.swift
@@ -0,0 +1,17 @@
+//
+//  Vita_HealthTests.swift
+//  Vita-HealthTests
+//
+//  Created by Qayyax on 8/23/25.
+//
+
+import Testing
+@testable import Vita_Health
+
+struct Vita_HealthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
