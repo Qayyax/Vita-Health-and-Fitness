fc6f0dd5b724500780b06972f4fe5929f398e2d8
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-08-24 07:10:48 -0400

    finished component

--- Vita-Health/Components/StatsProgress.swift
+++ Vita-Health/Components/StatsProgress.swift
@@ -11,35 +11,43 @@ struct StatsProgress: View {
   var icon: String = "person"
   var goalTitle: String = "Goal"
   var goalComment: String = "Comment"
-  var primaryColor: Color = .blue
-  var secondaryColor: Color = .blue
+  var primaryColor: Color = AppTheme.primaryGreen
+  var secondaryColor: Color = AppTheme.secondaryGreen
   var completed: Double = 0.5
   
     var body: some View {
       HStack {
         Image(systemName: icon)
+          .padding(10)
+          .font(.system(size: 24))
           .background(in: Circle())
-          .backgroundStyle(AppTheme.blue)
+          .foregroundStyle(primaryColor)
+          .backgroundStyle(secondaryColor)
         
         VStack(alignment: .leading) {
           Text(goalTitle)
-            .font(.caption)
-            .foregroundColor(.secondary)
+            .font(.title)
+            .foregroundColor(AppTheme.dark)
           Text(goalComment)
-            .font(.caption2)
+            .font(.title2)
+            .foregroundStyle(AppTheme.gray)
         }
+        
+        Spacer()
         
         VStack(alignment: .trailing) {
           ZStack(alignment: .leading) {
             RoundedRectangle(cornerRadius: 8)
-              .frame(width: 80, height: 10)
+              .frame(width: 100, height: 10)
               .foregroundStyle(AppTheme.offBlue)
             
             RoundedRectangle(cornerRadius: 8)
-              .frame(width: completed * 80, height: 10)
+              .frame(width: completed * 100, height: 10)
               .foregroundStyle(primaryColor)
           }
           Text("\(Int(completed * 100))%")
+            .fontWeight(.semibold)
+            .foregroundStyle(AppTheme.gray)
         }
       }
       .padding()
