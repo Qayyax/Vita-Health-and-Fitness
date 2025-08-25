5ddd47aa3200ec84b76e6787213c804a43e7dfef
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-08-24 20:27:37 -0400

    small text to color the top safearea

--- Vita-Health/Home.swift
+++ Vita-Health/Home.swift
@@ -12,69 +12,79 @@ struct Home: View {
     ZStack {
       AppTheme.offYellow
         .ignoresSafeArea()
-      ScrollView {
-        VStack {
-          HStack {
-            Text("Good Morning **Gizem!**")
-              .font(.title)
-            Spacer()
-            ProfileCircular()
-          }
-          .padding(.horizontal)
-          
-          // Vstack for into - would hide
-          HStack {
-            Image("morning")
-              .resizable()
-              .frame(width: 125, height: 150)
-              
-            
-            // For the text
-            VStack(alignment: .leading, spacing: 10) {
-              Text("You're doing great today!")
-                .font(.headline)
-              
-              HStack {
-                Text("Chat with VitAi")
-                  .fontWeight(.semibold)
-                Image(systemName: "sparkles")
-                  .foregroundStyle(AppTheme.primaryGreen)
-              }
-              .padding()
-              .background(Color.white)
-              .cornerRadius(20)
-              
-            }
-            Spacer()
-          }
-        }
-        .background(AppTheme.secondaryGreen)
-        .clipShape(
-          UnevenRoundedRectangle(cornerRadii: .init(
-            bottomLeading: 50,
-            bottomTrailing: 50,
-          ))
-        )
-        .padding(.bottom, 20)
-        
-        // Progress wheel section
+      VStack(spacing: 0) {
         HStack {
-          ProgressWheel(progress: 0.8)
           Spacer()
-          VStack (alignment: .leading){
-            ForEach(statsOverview, id: \.self.1) { stat in
-              StatsOverview(circleColor: stat.0, title: stat.1, currentValue: stat.2, goalValue: stat.3, unit: stat.4)
-            }
-            
-          }
+        Text("")
+          .padding(0)
         }
-        .padding(20)
-        .background(AppTheme.white)
-        .clipShape(RoundedRectangle(cornerRadius: 30))
-        .padding(.horizontal, 20)
+        .frame(width: .infinity)
+        .background(AppTheme.secondaryGreen)
 
-      }
+        ScrollView {
+          VStack {
+            HStack {
+              Text("Good Morning **Gizem!**")
+                .font(.title)
+              Spacer()
+              ProfileCircular()
+            }
+            .padding(.horizontal)
+            
+            // Vstack for into - would hide
+            HStack {
+              Image("morning")
+                .resizable()
+                .frame(width: 125, height: 150)
+              
+              
+              // For the text
+              VStack(alignment: .leading, spacing: 10) {
+                Text("You're doing great today!")
+                  .font(.headline)
+                
+                HStack {
+                  Text("Chat with VitAi")
+                    .fontWeight(.semibold)
+                  Image(systemName: "sparkles")
+                    .foregroundStyle(AppTheme.primaryGreen)
+                }
+                .padding()
+                .background(Color.white)
+                .cornerRadius(20)
+                
+              }
+              Spacer()
+            }
+          }
+          .background(AppTheme.secondaryGreen)
+          .clipShape(
+            UnevenRoundedRectangle(cornerRadii: .init(
+              bottomLeading: 50,
+              bottomTrailing: 50,
+            ))
+          )
+          .padding(.bottom, 20)
+          
+          // Progress wheel section
+          HStack {
+            ProgressWheel(progress: 0.8)
+            Spacer()
+            VStack (alignment: .leading){
+              ForEach(statsOverview, id: \.self.1) { stat in
+                StatsOverview(circleColor: stat.0, title: stat.1, currentValue: stat.2, goalValue: stat.3, unit: stat.4)
+              }
+              
+            }
+          }
+          .padding(20)
+          .background(AppTheme.white)
+          .clipShape(RoundedRectangle(cornerRadius: 30))
+          .padding(.horizontal, 20)
+          
+        }
       .border(AppTheme.primaryGreen, width: 2)
+      }
     }
   }
 }
