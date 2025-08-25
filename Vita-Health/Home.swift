b2bd713f3d6e63bc7647fcb06c202b2ca97af8b8
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-08-25 14:26:34 -0400

    small changes for the pinned

--- Vita-Health/Home.swift
+++ Vita-Health/Home.swift
@@ -14,41 +14,45 @@ struct Home: View {
       AppTheme.offWhite
         .ignoresSafeArea()
 
+      VStack {
+        VStack {
+          HStack {
+            Text(scrollPosition.y >= 0 ? "Good Morning **Gizem!**" : "**Home**")
+              .font(.title)
+            Spacer()
+            ProfileCircular()
+          }
+          .padding(.horizontal)
+          .safeAreaPadding(.top, 50)
+
+          // Vstack for into - would hide
+        }
+        .background(AppTheme.secondaryGreen)
+        .clipShape(
+          UnevenRoundedRectangle(cornerRadii: .init(
+            bottomLeading: 50,
+            bottomTrailing: 50,
+          ))
+        )
+        .padding(.bottom, 20)
+        
         ScrollView {
           VStack {
-            HStack {
-              Text(scrollPosition.y > 0 ? "Good Morning **Gizem!**" : "**Home**")
-                .font(.title)
-              Spacer()
-              ProfileCircular()
+            if scrollPosition.y >= 0 {
+              YouAreDoingGreat()
             }
-            .padding(.horizontal)
-            .safeAreaPadding(.top, 50)
-
-            // Vstack for into - would hide
-            VStack {
-              if scrollPosition.y > 0 {
-                YouAreDoingGreat()
-              }
-            }
-              .background(GeometryReader { geometry in
-                Color.clear
-                  .preference(key: ScrollOffsetPreferenceKey.self, value: geometry.frame(in: .named("scroll")).origin)
-              })
-              .onPreferenceChange(ScrollOffsetPreferenceKey.self) { value in
-                self.scrollPosition = value
-              }
           }
-          .background(AppTheme.secondaryGreen)
-          .clipShape(
-            UnevenRoundedRectangle(cornerRadii: .init(
-              bottomLeading: 50,
-              bottomTrailing: 50,
-            ))
-          )
-          .padding(.bottom, 20)
+            .background(GeometryReader { geometry in
+              Color.clear
+                .preference(key: ScrollOffsetPreferenceKey.self, value: geometry.frame(in: .named("scroll")).origin)
+            })
+            .onPreferenceChange(ScrollOffsetPreferenceKey.self) { value in
+              self.scrollPosition = value
+            }
+          
           
           // Progress wheel section
+          Text("\(scrollPosition.y)")
           GoalsCompletedOverall()
           
           HabitChallenge()
@@ -70,6 +74,8 @@ struct Home: View {
         .coordinateSpace(name: "scroll")
         .safeAreaPadding(.bottom, 50)
         .ignoresSafeArea()
+      }
+      .ignoresSafeArea()
     }
   }
 }
