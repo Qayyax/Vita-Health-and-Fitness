f18606bf273a3dc5d702762f073caaf14161ef4d
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-08-24 22:15:48 -0400

    habit challenge

---
+++ Vita-Health/Components/Home-Page-Components/HabitChallenge.swift
@@ -0,0 +1,54 @@
+//
+//  HabitChallenge.swift
+//  Vita-Health
+//
+//  Created by Qayyax on 8/24/25.
+//
+
+import SwiftUI
+
+struct HabitChallenge: View {
+    var body: some View {
+      HStack {
+        VStack (alignment: .leading){
+          HStack {
+            Text("21-Day Habit Challenge")
+            HStack(spacing: 0) {
+              Text("6")
+                .foregroundStyle(AppTheme.primaryGreen)
+              Text("/21")
+                .foregroundStyle(AppTheme.gray)
+            }
+          }
+          .fontWeight(.bold)
+          .padding(.bottom, 10)
+          Text("Drink 2L water")
+          HStack {
+            Text("Reveal Today's Surprise 🎁")
+              .fontWeight(.semibold)
+          }
+          .padding()
+          .background(AppTheme.secondaryGreen)
+          .clipShape(RoundedRectangle(cornerRadius: 20))
+        }
+        Spacer()
+      }
+      .padding()
+      .background(AppTheme.white)
+      .clipShape(RoundedRectangle(cornerRadius: 20))
+      .padding()
+      .overlay(alignment: .trailing) {
+        Image(systemName: "gift")
+          .resizable()
+          .fontWeight(.regular)
+          .foregroundStyle(AppTheme.secondaryGreen)
+          .rotationEffect(.degrees(-30))
+          .frame(width: 100, height: 100)
+          .opacity(0.3)
+      }
+    }
+}
+
+#Preview {
+    HabitChallenge()
+}
--- Vita-Health/Home.swift
+++ Vita-Health/Home.swift
@@ -39,43 +39,7 @@ struct Home: View {
           // Progress wheel section
           GoalsCompletedOverall()
           
-          HStack {
-            VStack (alignment: .leading){
-              HStack {
-                Text("21-Day Habit Challenge")
-                HStack(spacing: 0) {
-                  Text("6")
-                    .foregroundStyle(AppTheme.primaryGreen)
-                  Text("/21")
-                    .foregroundStyle(AppTheme.gray)
-                }
-              }
-              .fontWeight(.bold)
-              .padding(.bottom, 10)
-              Text("Drink 2L water")
-              HStack {
-                Text("Reveal Today's Surprise 🎁")
-                  .fontWeight(.semibold)
-              }
-              .padding()
-              .background(AppTheme.secondaryGreen)
-              .clipShape(RoundedRectangle(cornerRadius: 20))
-            }
-            Spacer()
-          }
-          .padding()
-          .background(AppTheme.white)
-          .clipShape(RoundedRectangle(cornerRadius: 20))
-          .padding()
-          .overlay(alignment: .trailing) {
-            Image(systemName: "gift")
-              .resizable()
-              .fontWeight(.regular)
-              .foregroundStyle(AppTheme.secondaryGreen)
-              .rotationEffect(.degrees(-30))
-              .frame(width: 100, height: 100)
-              .opacity(0.3)
-          }
+          HabitChallenge()
         }
         .ignoresSafeArea()
     }
